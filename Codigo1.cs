8f1023ffb90c1990b4fa55c50a8bab09bd728d89
Fiorella <7152afmb@gmail.com>
2026-02-18 18:53:37 -0500

    Actividad1_clase#

---
+++ Codigo1.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MyPersonalApp
+{
+    
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int edad = 17;
+            Console.WriteLine("Hola, soy Ana Fiorella y esta es mi edad " + edad);
+
+            //in edad = 16
+            string nombre = "Fiorella";
+            Console.WriteLine($"Hola, {nombre}, tienes {edad}.");
+        }
+    }
+}
