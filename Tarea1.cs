17144458ad19be58f8109723cdc5e1cc2dd99963
Fiorella <7152afmb@gmail.com>
2026-02-25 17:27:33 -0500

    Corregido ingreso de nombre y validaciones

--- Tarea1.cs
+++ Tarea1.cs
@@ -1,5 +1,16 @@
 using System;
 
-namespace bucles
+namespace MyPersonalApp
 {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Hola, porfavor ingresa tu nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine($"Hola, {nombre},por favor indicanos tu edad");
+            string edad = Console.ReadLine();
+            Console.WriteLine($"Hola {nombre}, tienes {edad} años");
+        }
+    }
 }
